84e08ec93d85a2a1aac40ecb02cc1e19ab975f9b
pklata <pawelklata@gmail.com>
2020-02-22 23:41:43 +0100

    prosty widok html generuje minimalna ilosc kodu html

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Listy rzeczy do zrobienia</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
