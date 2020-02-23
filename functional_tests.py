2f942f44fc5ff6709b0dc3de5344629154df9093
pklata <pawelklata@gmail.com>
2020-02-23 07:44:59 +0100

    test funkcjonalny sprawdza mozliwosc dodania elementow listy

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
-DEVELOPMENT_URL = "http://127.0.0.1:8000/"
+DEVELOPMENT_URL = 'http://127.0.0.1:8000/'
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,23 +20,37 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get(f'{DEVELOPMENT_URL}')
 
         # Zwróciła uwagę, że  tytuł strony i nagłówek zawierają słowo listy
-        self.assertIn("Listy", self.browser.title)
-        self.fail("Zakończenie testu!")
+        self.assertIn('Listy', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy', header_text)
 
         # Od razu zostaje zachęcona, aby wpisać rzecz do zrobienia
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Wpisz rzecz do zrobienia'
+        )
 
         # W polu tekstowym wpisałą kupić pawie pióra
+        inputbox.send_keys('Kupić pawie pióra')
 
         # Po wciśnięciu klawisza Enter strona zostąła uaktualniona i wyświetla
         # 1: Kupić pawie pióra jako element listy rzeczy do zrobienia
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Kupić pawie pióra' for row in rows)
+        )
 
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania
-        # Edyta wpisałą "Użyć pawich piór do zrobienia przynęty
+        # Edyta wpisałą 'Użyć pawich piór do zrobienia przynęty
+        self.fail(('Zakończenie testu'))
 
         # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia
 
         # Usatysfakcjonowana kładzie się spać
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
