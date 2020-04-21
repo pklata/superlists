6001796b7f69ec35b7f634902b26b7d033a81ca4
pklata <pawelklata@gmail.com>
2020-04-21 05:40:34 +0200

    updated deployment scripts

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -21,7 +21,7 @@ def _get_latest_source():
         run('git fetch')
     else:
         run(f'git clone {REPO_URL} .')
-    current_commit = local('git log -n 1 format=%H', capture=True)
+    current_commit = local('git log -n --pretty:"format:%H"" 1', capture=True)
     run(f'git reset -- hard {current_commit}')
 
 
