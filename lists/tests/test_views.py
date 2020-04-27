8a4828bebf196a31d5f6416ac62b43fd8cab4f74
pklata <pawelklata@gmail.com>
2020-04-27 20:16:49 +0200

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
+from lists.forms import ItemForm
 # Create your tests here.
 
 
@@ -13,6 +14,10 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class NewItemTest(TestCase):
