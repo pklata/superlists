0791412b53a19da618b74214a6a0c7bd5b77c6af
pklata <pawelklata@gmail.com>
2020-05-03 10:12:46 +0200

    send_mail mock

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import re_path, include
+from django.conf.urls import url, include
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as accounts_urls
 
 urlpatterns = [
-    re_path(r'^$', list_views.home_page, name='home'),
-    re_path(r'^lists/', include(list_urls)),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(accounts_urls)),
 ]
